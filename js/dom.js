ba09d73431fcb44daa4af1c28d7062a565541871
jjkzg <ivonaumo@outlook.com.ar>
2022-08-25 15:58:37 -0300

    trabajo terminado por el momento

---
+++ js/dom.js
@@ -0,0 +1,95 @@
+for(let t = 0;  t < TOBUY.length; t++){
+    let carr = document.createElement("div");
+    carr.innerHTML = `<h3> tela:  ${TOBUY[t].cloth} Total: $ ${TOBUY[t].price}</h3> `;
+    document.body.appendChild(carr);
+}
+
+
+//acá se crean las cards.
+function cargarProductos(){
+  for(let u=0; u < PRODUCTOS.length; u ++){
+      
+    subirProductos.innerHTML +=   `
+    <div class="card" style="width: 18rem;">
+    <img src="${PRODUCTOS[u].image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${PRODUCTOS[u].cloth}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${PRODUCTOS[u].compotition}</li>
+      <li class="list-group-item">$ ${PRODUCTOS[u].price} por m2</li>
+      <li class="list-group-item">COLORES</li>
+      <li class="list-group-item">Blanco - Gris - Negro</li>
+    </ul>
+  </div> ` 
+      
+  }
+}
+//función que crea en HTML espacios para cargar la tela que desea el usuario
+function elegirTela(){
+  cargarCortinas.innerHTML += `<select name="select" class="select">
+  <option class = "telaNo" value="value0" selected>...</option>
+  <option class = "telaBlackout" value="value1">${PRODUCTOS[0].cloth}</option>
+  <option class = "telaSunscreen" value="value2">${PRODUCTOS[1].cloth}</option>
+  <option class = "telaBanda" value="value3">${PRODUCTOS[2].cloth}</option>
+  <input class="btnHecho" type="submit" value= "hecho"></a>`
+  telaSeleccion()
+}
+
+
+//evento que cambia de color el título cuando ponemos el cursor arriba
+tituloAmarillo.addEventListener("mousemove", () => {
+    tituloAmarillo.className = "colorAmarillo"
+})
+
+//evento para agregar negro al nav cuando se scrollea
+window.onscroll = (() => {
+  const navScroll = document.querySelector("nav")
+  if (window.scrollY > 30) {
+    navScroll.className = "navbar navbar-dark bg-dark  fixed-top opacity"
+  } else {
+    navScroll.className = "navbar navbar-dark bg-dark fixed-top"
+  }
+});
+
+ cargarProductos()
+
+// funcion que lee la cantidad que quiere comprar y crea un input por cortina
+ function nvoCodigo(){  
+  const cuantasCortinas = document.querySelector(".cuantas")
+  const enviarCantidad = document.querySelector(".enviarCantidad")
+  enviarCantidad.addEventListener("click", (e) =>{
+    e.preventDefault()
+    const cortinasCantidad = parseInt(cuantasCortinas.value)
+    if(cortinasCantidad!= NaN || cortinasCantidad != Null){
+      for(let j = 0; j < cortinasCantidad; j++){
+        elegirTela()    
+        console.log(cortinasCantidad)
+      }
+    }    
+  });
+}
+
+//evento que lee el click en "cotizar" y crea un input que agrega cantidad de cortinas
+btnCotizar.addEventListener("click" , function addSelect(){
+  let add = document.querySelector("#agregarInputs")
+  add.innerHTML = add.innerHTML + `<input class = "cuantas" type ="number" placeholder = "cuántas cortinas desea comprar?" value = ""></a>
+  <input class="enviarCantidad" type="submit" value= "agregar al carrito"></a>`
+  nvoCodigo()
+})
+
+
+//función seleccionar tela
+function telaSeleccion(){
+  const btnHecho = document.querySelector(".btnHecho")
+        btnHecho.addEventListener("click", function medidasHtml(){  
+          const tela = document.querySelector(".select").value
+          if(tela != "value0"){
+            debugger
+            const agregarMedidas = document.querySelector("#agregarMedidas")
+            agregarMedidas.innerHTML += `</select><input class = largo type ="number" placeholder = "ingrese el ancho"></a>
+            <input class = ancho type ="number" placeholder = "ingrese el largo"></a>
+            <input type="submit" value= "agregar al carrito"></a>`
+          }
+        })
+}
---
+++ js/variables.js
@@ -0,0 +1,44 @@
+const buy = []
+const PRODUCTOS = []
+const TOBUY = []
+let windows = 0
+let tela = 0
+let l = 0
+let ancho = 0
+let totalPrice = 0
+let url = 0
+
+const subirProductos = document.querySelector("#productos")
+const tituloAmarillo = document.getElementById("titulo")
+const imgSubir = document.querySelector(".card-img-top")
+const btnCotizar = document.querySelector("#cotizar")
+const cargarCortinas = document.querySelector("#cargarCortinas")
+
+
+
+
+class Product{
+    constructor(order, cloth, compotition, image, price){
+        this.order = order
+        this.cloth = cloth
+        this.compotition = compotition
+        this.image = image
+        this.price = price
+    }
+    
+}
+
+function push(){
+PRODUCTOS.push(new Product("A", "BLACKOUT", "Patrón: Plane weave x peso: 5,50 +/- 5%(gr/m2)", "./img/blackout.jpg", 20))
+PRODUCTOS.push(new Product("D", "SUNSCREEN 5%","Patrón: Tatting", "./img/sunscreen.jpg",  27))
+PRODUCTOS.push(new Product("E", "BANDAS VERTICALES", "Blackout & Sunscreen 5%", "./img/bandaVertical.jpg", 40))
+localStorage.setItem("productos", JSON.stringify(PRODUCTOS))
+}
+push()
+
+function recuperarLs(){
+    if(localStorage.productos){
+        const prodLs = JSON.parse(localStorage.getItem("productos"))
+        console.table(prodLs)
+    }
+}
